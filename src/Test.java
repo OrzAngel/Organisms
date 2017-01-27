5e2940078b1d3cd10b819ddc68cd962237e637ea
OrzAngel <junche@seas.upenn.edu>
2017-01-26 19:14:15 -0500

    Test class updated

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Move m = new Move(1);
+		ArrayList<Player> pl = new ArrayList<>();
+		GameConfig game = new GameConfigImp(1,20,10,500,10);
+		OrganismsGameInterface og = new OrganismsGameImp();
+
+		
+		Player p =new RandomPlayer();
+		p.register(game, 555);
+
+		pl.add(p);
+		og.initialize(game, 0.001, 0.002, pl);
+		og.playGame();
+		
 	}
 
 }
